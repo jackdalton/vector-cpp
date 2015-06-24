8868b0a396c7036a4bf77308947b2a640fbd3a9f
Jack Dalton <jackdalton00@gmail.com>
2015-06-24 13:08:35 -0600

    Converted to a Code::Blocks project; added midpoint functions; added TODO.md.

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# To Do
+
+* Nothing, for now.
--- src/vector.h
+++ src/vector.h
@@ -17,6 +17,12 @@ public:
     double distanceTo(Vector2 pos) {
         return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x));
     }
+    Vector2 midpoint(Vector2 pos) {
+        double mx = (x + pos.x) / 2;
+        double my = (y + pos.y) / 2;
+        Vector2 mid(mx, my);
+        return mid;
+    }
 };
 class Vector3 {
 public:
@@ -31,4 +37,12 @@ public:
     double distanceTo(Vector3 pos) {
         return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x) + (pos.z - z) * (pos.z - z));
     }
+    Vector3 midpoint(Vector3 pos) {
+        double mx = (x + pos.x) / 2;
+        double my = (y + pos.y) / 2;
+        double mz = (z + pos.z) / 2;
+        Vector3 mid(mx, my, mz);
+        return mid;
+    }
 };
+
